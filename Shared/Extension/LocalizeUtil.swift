2a63a3cbb71f6c4035eaa82abd57049c83bc999d
hagemon <hagemon>
2021-11-20 22:36:04 +0800

    Localized & Fix Introduction issue

---
+++ Shared/Extension/LocalizeUtil.swift
@@ -0,0 +1,8 @@
+//
+//  LocalizeUtil.swift
+//  TimeTo
+//
+//  Created by Hagemon on 2021/11/20.
+//
+
+import Foundation
---
+++ Shared/Views/Settings/AcknowledgeView.swift
@@ -0,0 +1,20 @@
+//
+//  AcknowledgeView.swift
+//  TimeTo
+//
+//  Created by Hagemon on 2021/11/20.
+//
+
+import SwiftUI
+
+struct AcknowledgeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AcknowledgeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AcknowledgeView()
+    }
+}
